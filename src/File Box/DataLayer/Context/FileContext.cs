39d1aa03a25fa9739197100f26d474c197768af4
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-07 21:29:30 +0330

    update Layers

---
+++ src/File Box/DataLayer/Context/FileContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class FileContext : DbContext
+{
+    public FileContext(DbContextOptions<FileContext> options):base(options)
+    {
+
+    }
+}
