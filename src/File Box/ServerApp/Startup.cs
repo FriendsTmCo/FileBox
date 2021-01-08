589996b4443a4317126405525138b66875cc52ad
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-08 13:05:36 +0330

    Insert View Models Update StartUp

--- src/File Box/ServerApp/Startup.cs
+++ src/File Box/ServerApp/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,9 @@ namespace ServerApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<FileContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("FileConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
