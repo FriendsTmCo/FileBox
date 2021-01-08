f29f6019f3efdd94c38278d77fa321c519291bbd
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-08 13:42:23 +0330

    Create File Services

---
+++ src/File Box/Services/Repository/ICrudRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+    /// <summary>
+    /// Crud Repository Services 
+    /// </summary>
+    /// <typeparam name="T">T Model</typeparam>
+    public interface ICrudRepository<T>
+    {
+        /// <summary>
+        /// Return All Model 
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetAllAsync();
+
+        /// <summary>
+        /// Return All Model With Expression
+        /// </summary>
+        /// <param name="where">Expression</param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T,bool>> where);
+
+        /// <summary>
+        /// Return All Model with Search
+        /// </summary>
+        /// <param name="q">Search Parameter</param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetBySearchAsync(string q);
+
+        /// <summary>
+        /// Fidn Model 
+        /// </summary>
+        /// <param name="Id">Model Id</param>
+        /// <returns></returns>
+        Task<T> GetByIdAsync(object Id);
+
+        /// <summary>
+        /// Insert New Model To Data Base
+        /// </summary>
+        /// <param name="tModel">T Model</param>
+        /// <returns></returns>
+        Task<bool> InsertAsync(T tModel);
+
+        /// <summary>
+        /// Update Model 
+        /// </summary>
+        /// <param name="tModel">T Model</param>
+        /// <returns></returns>
+        Task<bool> UpdateAsync(T tModel);
+
+        /// <summary>
+        /// Delete Model 
+        /// </summary>
+        /// <param name="tModel">T Model</param>
+        /// <returns></returns>
+        Task<bool> DeleteAsync(T tModel);
+
+        /// <summary>
+        /// Delete Model With Id
+        /// </summary>
+        /// <param name="Id">T Model Id</param>
+        /// <returns></returns>
+        Task<bool> DeleteAsync(object Id);
+
+        /// <summary>
+        /// Save All changes Async
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> SaveAsync();
+
+        /// <summary>
+        /// Save All Changes
+        /// </summary>
+        /// <returns></returns>
+        bool Save();
+    }
+}
---
+++ src/File Box/Services/Services/FilesServices.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class FilesServices : IFileRepository, ICrudRepository<Files>, IDisposable
+    {
+        #region :: Depenedency::
+
+        private readonly FileContext _db;
+
+        public FilesServices(FileContext db)
+        {
+            _db = db;
+        }
+
+        #endregion
+
+        public async Task<bool> DeleteAsync(Files tModel)
+        {
+            return await Task.Run(() =>
+            {
+                try
+                {
+                    _db.Files.Remove(tModel);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            });
+        }
+
+        public async Task<bool> DeleteAsync(object Id)
+        {
+            return await Task.Run(async () => await DeleteAsync(GetByIdAsync(Id)));
+        }
+
+        public async void Dispose()
+        {
+            await _db.DisposeAsync();
+        }
+
+        public async Task<IEnumerable<Files>> GetAllAsync()
+        {
+            return await Task.Run(async () => await _db.Files.ToListAsync());
+        }
+
+        public async Task<IEnumerable<Files>> GetAllAsync(Expression<Func<Files, bool>> where)
+        {
+            return await Task.Run(async () => await _db.Files.Where(where).ToListAsync());
+        }
+
+        public async Task<Files> GetByIdAsync(object Id)
+        {
+            return await Task.Run(async () => await _db.Files.FindAsync(Id));
+        }
+
+        public async Task<IEnumerable<Files>> GetBySearchAsync(string q)
+        {
+            return await Task.Run(async () => await GetAllAsync(f => f.FileName.Contains(q)));
+        }
+
+        public async Task<Files> GetFileByDownloadLinkAsync(string downloadLink)
+        {
+            return await Task.Run(async () => await _db.Files.FirstOrDefaultAsync(f => f.DownloadLink == downloadLink));
+        }
+
+        public async Task<bool> InsertAsync(Files tModel)
+        {
+            return await Task.Run(async () =>
+            {
+                try
+                {
+                    await _db.Files.AddAsync(tModel);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            });
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                _db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await Task.Run(async () =>
+            {
+                try
+                {
+                    await _db.SaveChangesAsync();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            });
+        }
+
+        public async Task<bool> UpdateAsync(Files tModel)
+        {
+            return await Task.Run( () =>
+            {
+                try
+                {
+                    _db.Files.Update(tModel);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            });
+        }
+    }
+}
