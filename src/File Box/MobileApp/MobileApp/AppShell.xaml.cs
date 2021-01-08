9a4fa1b82967ac87487d9ff397b5164a27b31ec8
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-08 12:33:32 +0330

    insert mobile

---
+++ src/File Box/MobileApp/MobileApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MobileApp.ViewModels;
+using MobileApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ src/File Box/MobileApp/MobileApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MobileApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MobileApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
