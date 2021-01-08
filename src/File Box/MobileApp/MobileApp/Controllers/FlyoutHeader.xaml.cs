b9456bbaebc485ee3e3cd03b5ac261badf2b2af1
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-08 17:54:11 +0330

    Start Mobile App

---
+++ src/File Box/MobileApp/MobileApp/Controllers/FlyoutHeader.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Controllers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlyoutHeader : ContentPage
+    {
+        public FlyoutHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
