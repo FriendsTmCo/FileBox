c1cc5bbd5fc607371f37b3d264f08bceba09f789
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-10 00:01:56 +0330

    Update Mobile Layer

--- src/File Box/MobileApp/MobileApp/AppShell.xaml.cs
+++ src/File Box/MobileApp/MobileApp/AppShell.xaml.cs
@@ -13,6 +13,7 @@ namespace MobileApp
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
